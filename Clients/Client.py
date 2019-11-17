3a6dfd70b3acf3ff0441f49c54294d0700d5498c
Leczez <linus.classon@hotmail.com>
2019-11-17 15:10:58 +0100

    fixed client.py

--- Clients/Client.py
+++ Clients/Client.py
@@ -8,9 +8,9 @@ host = "192.168.0.111"
 connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connection.connect((host, port))
 
-file = open("ThePool", "r")
-
-while connection.send("Hus:".encode("utf-8") + file.encode("utf-8")):
+while True:
+    file = open("ThePool", "r")
+    connection.send("Hus:".encode("utf-8") + file.encode("utf-8"))
     print(file)
+    file.close()
 
-file.close()
