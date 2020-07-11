4d792b55adfe1dd601a587e79ed0f7c130305196
Leczez <linus.classon@hotmail.com>
2020-07-11 21:46:12 +0200

    POST request now works

--- Raspberry Pi Webserver/server.js
+++ Raspberry Pi Webserver/server.js
@@ -4,10 +4,110 @@ var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
+var host = '192.168.0.84';
+var port = 8080;
+
+var index = fs.readFileSync("index.html");
+var file = fs.readFileSync("style.css");
+
+
+function addDevice(req, res, body)
+{
+    try 
+    {
+        console.log(body);
+        var data = JSON.parse(body);
+        console.log("data: " + JSON.stringify(data));
+      
+        fs.readFile("device_register.json", adding);
+        
+        function adding(err, device_register)
+        {
+            if(err)
+            { 
+                console.log(err);
+                throw err;
+            }else
+            {
+                console.log("file read");
+            }
+            
+            var json = JSON.parse(device_register);
+            console.log(json);
+
+            var keys = Object.keys(data);
+            var word = keys[0];
+
+            json[word] = data[word];
+
+            console.log(json);
+            //console.log(data);
+            //console.log(keys[0]);
+            
+
+            /*
+            try
+            {
+                fs.writeFile("testy.json", JSON.stringify(json, null, 2), function(err)
+                {
+                    if(err)
+                    { 
+                        console.log(err);
+                        throw err;
+                    }else
+                    {
+                        console.log("The file has been saved");
+                    }
+                });    
+            } catch (error)
+            {
+                console.log(error);
+                throw error;    
+            }
+            */
+        }
+        
+        
+
+        
+        res.statusCode = 200;
+        res.end("POST Successful");
+
+    } catch (error) 
+    {
+        console.log("error: " + error.message);
+        res.statusCode = 400;
+        res.writeHead(400);
+        res.end("error: " + error.message);
+    }
+
+}
 
 function POST(req, res)
 {
-
+    var body = "";
+    req.on('data', function(data)
+    {   
+        body += data;
+        //console.log(data);
+        //console.log(data.toString());
+    });
+    req.on('end',() => 
+    {
+        switch(req.url)
+        {
+            case "/devices":
+                addDevice(req, res, body);
+        
+            break;
+            
+            default:
+                res.statusCode = 400;
+                res.write("not a valid POST request");
+                res.end();
+        }
+  
+    });
 }
 
 function GET(req, res, q)
@@ -15,32 +115,42 @@ function GET(req, res, q)
     switch(q.pathname)
     {
     case "/devices":
-        var register = fs.readFileSync("device_register.json");
-        res.writeHead(200,{"Content-Type": "text/html"})
-        res.write(register);
+        fs.readFile("device_register.json", devices);
+        function devices(err, data)
+        {
+            if(err)
+            {
+                console.log("error: " + err);
+            }else
+            {
+                res.setHeader("Content-Type", "application/json");
+                res.writeHead(200);
+                res.write(data);
+                res.end();
+                //console.log(register);
+            }
+        }
+        
         break;
     
     case "/style.css":
-        var file = fs.readFileSync("style.css");
-        res.writeHead(200,{"Content-Type": "text/css"});
+        res.setHeader("Content-Type", "text/css");
+        res.writeHead(200);
         res.write(file);
+        res.end();
         break;
 
     default:
-        var content = fs.readFileSync("index.html");
-        res.writeHead(200,{"Content-Type": "text/html"})
-        res.write(content);
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(200);
+        res.write(index);
+        res.end();
 
     }
 }
 
 
-
-
-
-
-//Create the http server object
-http.createServer(function(req, res)
+const request_handler = function(req, res)
 {
     var q = url.parse(req.url, true);
     if(req.method == "POST")
@@ -51,7 +161,12 @@ http.createServer(function(req, res)
     {
         GET(req, res, q);
     }
-    
-    return res.end();
+}
 
-}).listen(8080);
+
+
+//Create the http server object
+const server = http.createServer(request_handler).listen(port, host,()=>
+{
+    console.log(`Server is running on http://${host}:${port}`);
+});
---
+++ Raspberry Pi Webserver/test.js
@@ -0,0 +1,15 @@
+var x = 10;
+
+
+
+
+function addone(input)
+{
+    input = input + 1;
+}
+
+
+
+
+addone(x);
+console.log(x);
